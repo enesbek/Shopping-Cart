076ba4e22b61aa84bac2db03dae6b73a4a96ac06
ENES BEK <enes.bek@stu.fsm.edu.tr>
2021-11-29 13:40:58 +0300

    acceptance start

---
+++ acceptance/specs/example.spec
@@ -0,0 +1,15 @@
+# Getting Started with Gauge
+
+This is an example markdown specification file.
+Every heading in this file is a scenario.
+Every bulleted point is a step.
+
+To execute this specification, use
+	npm test
+
+This is a context step that runs before every scenario
+* Open shopping-cart application
+
+## Add product to basket 
+* Open google
+
---
+++ acceptance/tests/step_implementation.js
@@ -0,0 +1,52 @@
+/* globals gauge*/
+"use strict";
+const path = require('path');
+const {
+    openBrowser,
+    write,
+    closeBrowser,
+    goto,
+    press,
+    screenshot,
+    above,
+    click,
+    checkBox,
+    listItem,
+    toLeftOf,
+    link,
+    text,
+    into,
+    textBox,
+    evaluate
+} = require('taiko');
+const assert = require("assert");
+const headless = process.env.headless_chrome.toLowerCase() === 'true';
+
+beforeSuite(async () => {
+    await openBrowser({
+        headless: headless
+    })
+});
+
+afterSuite(async () => {
+    await closeBrowser();
+});
+
+// Return a screenshot file name
+gauge.customScreenshotWriter = async function () {
+    const screenshotFilePath = path.join(process.env['gauge_screenshots_dir'],
+        `screenshot-${process.hrtime.bigint()}.png`);
+
+    await screenshot({
+        path: screenshotFilePath
+    });
+    return path.basename(screenshotFilePath);
+};
+
+step("Open shopping-cart application", async () => {
+    await goto("localhost:3000");
+});
+
+step("Open google", async () => {
+    await goto("www.google.com");
+});
