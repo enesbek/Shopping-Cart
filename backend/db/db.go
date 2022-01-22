befd2e95224ef7485927de035dfab8e1da33174b
ENES BEK <enes.bek@stu.fsm.edu.tr>
2022-01-22 13:07:50 +0300

    Update db.go

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,13 +2,17 @@ package db
 
 import (
 	"backend/constants"
+	"fmt"
+	"os"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 func Connect() *sqlx.DB {
-	db, err := sqlx.Open("postgres", "host=db port=5432 user=postgres dbname=postgres password=1234 sslmode=disable")
+	dbHost := os.Getenv("DBHOST")
+	connectionString := fmt.Sprintf("host=%s port=5432 user=postgres dbname=postgres password=1234 sslmode=disable", dbHost)
+	db, err := sqlx.Open("postgres", connectionString)
 
 	if err != nil {
 		panic(err)
